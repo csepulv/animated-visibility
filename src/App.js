6a2ddec33d83ccf61669f2b3b52dbe8c111f9ccb
Christian Sepulveda <csepulv@gmail.com>
2019-07-09 20:36:23 -0700

    add animation, leaves holes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import superb from "superb";
+import { Animated } from "react-animated-css";
 import "./App.css";
 
 const colors = [
@@ -18,20 +19,21 @@ function Box({ word }) {
   const color = colors[Math.floor(Math.random() * 9)];
   const [visible, setVisible] = useState(true);
 
-  function hideMe(){
+  function hideMe() {
     setVisible(false);
   }
 
   let style = { borderColor: color, backgroundColor: color };
-  if (!visible) style.display = "none";
 
   return (
-    <div className="box" style={style}>
-      <div className="center">{word}</div>
-      <button className="button bottom-corner" onClick={hideMe}>
-        <i className="center far fa-eye fa-lg" />
-      </button>
-    </div>
+    <Animated animationIn="zoomIn" animationOut="zoomOut" isVisible={visible}>
+      <div className="box" style={style}>
+        <div className="center">{word}</div>
+        <button className="button bottom-corner" onClick={hideMe}>
+          <i className="center far fa-eye fa-lg" />
+        </button>
+      </div>
+    </Animated>
   );
 }
 
