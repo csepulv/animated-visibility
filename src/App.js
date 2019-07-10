0fc05712840e5c55100678650356615182c0d77e
Christian Sepulveda <csepulv@gmail.com>
2019-07-09 20:48:56 -0700

    add animation, no holes (use timeout)

--- src/App.js
+++ src/App.js
@@ -18,15 +18,22 @@ const colors = [
 function Box({ word }) {
   const color = colors[Math.floor(Math.random() * 9)];
   const [visible, setVisible] = useState(true);
+  const [fading, setFading] = useState(false);
 
   function hideMe() {
-    setVisible(false);
+    setFading(true);
+    setTimeout(() => setVisible(false), 650);
   }
 
   let style = { borderColor: color, backgroundColor: color };
 
   return (
-    <Animated animationIn="zoomIn" animationOut="zoomOut" isVisible={visible}>
+    <Animated
+      animationIn="zoomIn"
+      animationOut="zoomOut"
+      isVisible={!fading}
+      style={visible ? null : { display: "none" }}
+    >
       <div className="box" style={style}>
         <div className="center">{word}</div>
         <button className="button bottom-corner" onClick={hideMe}>
