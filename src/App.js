b622caa4d27588e7ff52f44c0ed6820d5fa51723
Christian Sepulveda <csepulv@gmail.com>
2019-07-09 18:34:18 -0700

    baseline
    add hideMe button and action
    
    assign key to boxes
    
    remove animation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import superb from "superb";
+import "./App.css";
+
+const colors = [
+  "#6690FF",
+  "#6CD566",
+  "#50E5FF",
+  "#FFDC75",
+  "#FF7C83",
+  "#FF702D",
+  "#FFAA42",
+  "#7F7B82",
+  "#4D7EA8"
+];
+
+function Box({ word }) {
+  const color = colors[Math.floor(Math.random() * 9)];
+  const [visible, setVisible] = useState(true);
+
+  function hideMe(){
+    setVisible(false);
+  }
+
+  let style = { borderColor: color, backgroundColor: color };
+  if (!visible) style.display = "none";
+
+  return (
+    <div className="box" style={style}>
+      <div className="center">{word}</div>
+      <button className="button bottom-corner" onClick={hideMe}>
+        <i className="center far fa-eye fa-lg" />
+      </button>
+    </div>
+  );
+}
 
 function App() {
+  const words = [];
+  for (let i = 0; i < 12; i++) {
+    words[i] = superb.random();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="frame">
+      {words.map(word => (
+        <Box key={word} word={word} />
+      ))}
     </div>
   );
 }
